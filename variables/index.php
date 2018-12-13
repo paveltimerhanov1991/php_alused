a76e1d9b3578984072b0ccd8eb5f277fef94b8d8
pavel.timerhanov <pavel.timerhanov@khk.ee>
2018-12-13 15:51:51 +0200

    Muutujad ja tehted

--- variables/index.php
+++ variables/index.php
@@ -21,10 +21,21 @@ echo gettype($sone).'<br>';
 $arv1 = 7;
 $arv2 = 24;
 
+echo '<h4>Matemaatilised operaatorid</h4>';
 echo $arv1. ' + ' .$arv2. ' = ' .($arv1 + $arv2).'<br>';
 echo $arv1. ' - ' .$arv2. ' = ' .($arv1 - $arv2).'<br>';
 echo $arv1. ' * ' .$arv2. ' = ' .($arv1 * $arv2).'<br>';
 echo $arv1. ' / ' .$arv2. ' = ' .($arv1 / $arv2).'<br>';
 echo $arv1. ' % ' .$arv2. ' = ' .($arv1 % $arv2).'<br>';
+
+echo '<hr>';
+
+echo '<h4>Võrdlusoperaatorid</h4>';
+echo $arv1. ' < ' .$arv2. ' see on ' .($arv1 < $arv2).'<br>';
+echo $arv1. ' > ' .$arv2. ' see on ' .($arv1 > $arv2).'<br>';
+echo $arv1. ' <= ' .$arv2. ' see on ' .($arv1 <= $arv2).'<br>';
+echo $arv1. ' >= ' .$arv2. ' see on ' .($arv1 >= $arv2).'<br>';
+echo $arv1. ' == ' .$arv2. ' see on ' .($arv1 == $arv2).'<br>';
+echo $arv1. ' != ' .$arv2. ' see on ' .($arv1 != $arv2).'<br>';
 
 
