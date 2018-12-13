5dd930e122a6991a2de400c1972c1358abe24c01
pavel.timerhanov <pavel.timerhanov@khk.ee>
2018-12-13 16:12:36 +0200

    värvid

--- conditions/index.php
+++ conditions/index.php
@@ -11,17 +11,19 @@ echo '<h4>Tingimuslaused</h4>';
 
 
 $arv = rand(0,100);
-
-if($arv > 0 and $arv < 25) {
+$varv = ' ';
+if($arv >= 0 and $arv < 25) {
     // roheline
-    echo '<p style="color: green;">'.$arv. '<br>';
-} else if ($arv > 25 and $arv < 50){
+    $varv = 'green';
+} else if ($arv >= 25 and $arv < 50){
     // punane
-    echo '<p style="color: red;">'.$arv. '<br>';
-}else if ($arv > 50 and $arv < 75){
+    $varv = 'red';
+}else if ($arv >= 50 and $arv < 75){
     // sinine
-    echo '<p style="color: blue;">'.$arv. '<br>';
-}else if ($arv > 75 and $arv < 100){
+    $varv = 'blue';
+}else if ($arv >= 75 and $arv < 100){
     // lilla
-    echo '<p style="color: purple;">'.$arv. '<br>';
+    $varv = 'purple';
 }
+
+echo '<p style="color: '.$varv.' "> '.$arv.'</p>';
