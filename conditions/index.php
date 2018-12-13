98767865412afff0a2e75e5d13d552ed9820e360
pavel.timerhanov <pavel.timerhanov@khk.ee>
2018-12-13 16:06:49 +0200

    Tingimuslaused

--- conditions/index.php
+++ conditions/index.php
@@ -12,9 +12,16 @@ echo '<h4>Tingimuslaused</h4>';
 
 $arv = rand(0,100);
 
-if($arv % 2 == 0) {
-    // antud arv on paaris ja on roheline
+if($arv > 0 and $arv < 25) {
+    // roheline
     echo '<p style="color: green;">'.$arv. '<br>';
-} else {
+} else if ($arv > 25 and $arv < 50){
+    // punane
     echo '<p style="color: red;">'.$arv. '<br>';
+}else if ($arv > 50 and $arv < 75){
+    // sinine
+    echo '<p style="color: blue;">'.$arv. '<br>';
+}else if ($arv > 75 and $arv < 100){
+    // lilla
+    echo '<p style="color: purple;">'.$arv. '<br>';
 }
