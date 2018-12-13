d2ca1ab194128c048a66c057d61526b9102f98af
pavel.timerhanov <pavel.timerhanov@khk.ee>
2018-12-13 15:09:05 +0200

    Väljastus print ja echo

--- print/index.php
+++ print/index.php
@@ -1,3 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <title>php alused</title>
+    </head>
+    <body>
+    <h4>Väljastus</h4>
+
+
+
+
 <?php
 /**
  * Created by PhpStorm.
@@ -6,9 +17,12 @@
  * Time: 14:49
  */
 //teksti väljastus print käsuga
-print 'See on esimene lause php keeles<br>';
-print 'See on teine lause php keeles<br>';
+
 //  teksti väljastu echo käsuga
 
-echo 'See tekst on echo väljastus<br>';
+echo 'See tekst on HTML sees<br>';
 
+
+?>
+    </body>
+</html>
